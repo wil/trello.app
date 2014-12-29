70304c19674fcafd7f79da6351a74b836aa05845
Wil Tan <wil@dready.org>
2014-12-30 00:13:51 +1100

    update nw to v0.11.5, fix copy/paste and quit

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-NW_VER=0.8.3
-NW_ZIP=node-webkit-v$NW_VER-osx-ia32.zip
-NW_URL=https://s3.amazonaws.com/node-webkit/v$NW_VER/$NW_ZIP
+NW_VER=0.11.5
+NW_BASENAME=node-webkit-v$NW_VER-osx-x64
+NW_ZIP=$NW_BASENAME.zip
+NW_URL=http://dl.node-webkit.org/v$NW_VER/$NW_ZIP
 APP_DIR=Trello.app
 INNER_APP_DIR="$APP_DIR"/Contents/Resources/app.nw
 
 if [ ! -e "$NW_ZIP" ]; then
-	curl -o "$NW_ZIP" $NW_URL || exit 1
+	curl -L -o "$NW_ZIP" $NW_URL || exit 1
 fi
 
 if [ ! -e "$APP_DIR" ]; then
-	unzip "$NW_ZIP" 'node-webkit.app/*' || exit 1
-	mv node-webkit.app "$APP_DIR" || exit 1
+	unzip "$NW_ZIP" "$NW_BASENAME/node-webkit.app/*" || exit 1
+	mv "$NW_BASENAME/node-webkit.app" "$APP_DIR" || exit 1
 fi
 
 cp trello.icns "$APP_DIR"/Contents/Resources/nw.icns
